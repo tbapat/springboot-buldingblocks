82c8b2fa16e6937535be81fcb4151dfef01c6541
Tushar Vishwas Bapat <tbapat@altimetrik.com>
2022-08-09 15:11:21 -0300

    First Commit

---
+++ src/test/java/com/example/restservices/SpringbootBuildingblocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBuildingblocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
